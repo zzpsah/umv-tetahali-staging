3e0b20a63004400a0856d0a96e8f0ace8e4cce4d
PRASHANT KUMAR SAH <zzpscode@gmail.com>
2026-09-10 00:08:54 +0530

    refactor: decouple document archive data and layout

---
+++ assets/document-archive-config.js
@@ -0,0 +1,5 @@
+window.UMV_DOCUMENT_ARCHIVE_CONFIG = Object.freeze({
+  supabaseUrl: 'https://sxfnrwugsyfypqgfglzc.supabase.co',
+  publishableKey: 'sb_publishable_NARdPew6T6Zk8gYfxY3Hog_G7jcceKw',
+  recordLimit: 1000
+});
